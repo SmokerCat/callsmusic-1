ba501abf3908648f3b9c4d03ca234e927ab085d5
Smoker Cat <80395140+SmokerCat@users.noreply.github.com>
2021-05-19 15:11:38 +0530

    Update admins.py

--- callsmusic/handlers/admins.py
+++ callsmusic/handlers/admins.py
@@ -1,125 +1,109 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from asyncio import QueueEmpty
 
 from pyrogram import Client
 from pyrogram.types import Message
 
 from .. import queues
-
 from ..callsmusic import callsmusic
-from ..helpers.filters import command, other_filters
-from ..helpers.decorators import errors, authorized_users_only
+from ..helpers.chat_id import get_chat_id
+from ..helpers.decorators import authorized_users_only
+from ..helpers.decorators import errors
+from ..helpers.filters import command
+from ..helpers.filters import other_filters
 
 
-@Client.on_message(command("pause") & other_filters)
+@Client.on_message(command('pause') & other_filters)
 @errors
 @authorized_users_only
 async def pause(_, message: Message):
     (
-        await message.reply_text("Paused!")
+        await message.reply_text('<b>⏸ Paused</b>', False)
     ) if (
-        callsmusic.pause(message.chat.id)
+        callsmusic.pause(get_chat_id(message.chat))
     ) else (
-        await message.reply_text("Nothing i started yet!")
+        await message.reply_text('<b>❌ Nothing is playing</b>', False)
     )
 
 
-@Client.on_message(command("resume") & other_filters)
+@Client.on_message(command('resume') & other_filters)
 @errors
 @authorized_users_only
 async def resume(_, message: Message):
     (
-        await message.reply_text("Resumed!")
+        await message.reply_text('<b>▶️ Resumed</b>', False)
     ) if (
-        callsmusic.resume(message.chat.id)
+        callsmusic.resume(get_chat_id(message.chat))
     ) else (
-        await message.reply_text("Nothing here to Pause!")
+        await message.reply_text('<b>❌ Nothing is paused</b>', False)
     )
 
 
-@Client.on_message(command("stop") & other_filters)
+@Client.on_message(command('stop') & other_filters)
 @errors
 @authorized_users_only
 async def stop(_, message: Message):
-    if message.chat.id not in callsmusic.active_chats:
-        await message.reply_text("Nothing here to Play!")
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.active_chats:
+        await message.reply_text('<b>❌ Nothing is playing</b>', False)
     else:
         try:
-            queues.clear(message.chat.id)
+            queues.clear(chat_id)
         except QueueEmpty:
             pass
+        await callsmusic.stop(chat_id)
+        await message.reply_text('<b>⏹ Stopped streaming</b>', False)
 
-        await callsmusic.stop(message.chat.id)
-        await message.reply_text("Cleared the queue and left the call!")
 
-
-@Client.on_message(command("skip") & other_filters)
+@Client.on_message(command('skip') & other_filters)
 @errors
 @authorized_users_only
 async def skip(_, message: Message):
-    if message.chat.id not in callsmusic.active_chats:
-        await message.reply_text("Nothing here to play!")
+    chat_id = get_chat_id(message.chat)
+    if chat_id not in callsmusic.active_chats:
+        await message.reply_text('<b>❌ Nothing is playing</b>', False)
     else:
-        queues.task_done(message.chat.id)
-
-        if queues.is_empty(message.chat.id):
-            await callsmusic.stop(message.chat.id)
+        queues.task_done(chat_id)
+        if queues.is_empty(chat_id):
+            await callsmusic.stop(chat_id)
         else:
             await callsmusic.set_stream(
-                message.chat.id, queues.get(message.chat.id)["file"]
+                chat_id,
+                queues.get(chat_id)['file'],
             )
+        await message.reply_text('<b>✅ Skipped</b>', False)
 
-        await message.reply_text("Skipped!")
 
-
-@Client.on_message(command("mute") & other_filters)
+@Client.on_message(command('mute') & other_filters)
 @errors
 @authorized_users_only
 async def mute(_, message: Message):
-    result = callsmusic.mute(message.chat.id)
-
+    result = callsmusic.mute(get_chat_id(message.chat))
     (
-        await message.reply_text("Muted!")
+        await message.reply_text('<b>✅ Muted</b>', False)
     ) if (
         result == 0
     ) else (
-        await message.reply_text("Yeah I am Muted!")
+        await message.reply_text('<b>❌ Already muted</b>', False)
     ) if (
         result == 1
     ) else (
-        await message.reply_text("I am Not in Voice Chat!")
+        await message.reply_text('<b>❌ Not in call</b>', False)
     )
 
 
-@Client.on_message(command("unmute") & other_filters)
+@Client.on_message(command('unmute') & other_filters)
 @errors
 @authorized_users_only
 async def unmute(_, message: Message):
-    result = callsmusic.unmute(message.chat.id)
-
+    result = callsmusic.unmute(get_chat_id(message.chat))
     (
-        await message.reply_text("Unmuted!")
+        await message.reply_text('<b>✅ Unmuted</b>', False)
     ) if (
         result == 0
     ) else (
-        await message.reply_text("I can Sing!")
+        await message.reply_text('<b>❌ Not muted</b>', False)
     ) if (
         result == 1
     ) else (
-        await message.reply_text("I am not in voice chat!")
+        await message.reply_text('<b>❌ Not in call</b>', False)
     )
